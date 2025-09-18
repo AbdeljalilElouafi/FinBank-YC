40ba5f17fa4667f8e13634157e17a08b7ed31636
abdeljalilelouafi <abdeljalilelouafi2@gmail.com>
2025-09-18 19:22:43 +0100

    Created the enums for account type and transaction types

---
+++ src/main/java/com/finbank/model/TypeCompte.java
@@ -0,0 +1,6 @@
+package main.java.com.finbank.model;
+
+public enum TypeCompte {
+    COURANT,
+    EPARGNE
+}
---
+++ src/main/java/com/finbank/model/TypeTransaction.java
@@ -0,0 +1,7 @@
+package main.java.com.finbank.model;
+
+public enum TypeTransaction {
+    DEPOT,
+    RETRAIT,
+    VIREMENT
+}
